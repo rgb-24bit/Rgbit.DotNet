139aedaa85f3185602113dbab74f1913abc8cb9f
rgb-24bit <rgb-24bit@foxmail.com>
2018-09-18 18:54:11 +0800

    feat: add WinFormsUtils

---
+++ Rgbit/DotNet/WinFormsUtils/ControlUtils.cs
@@ -0,0 +1,94 @@
+﻿/**
+ * Copyright (c) 2018 by rgb-24bit.
+ * License MIT, see LICENSE for more details.
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace Rgbit.DotNet.WinFormsUtils
+{
+    /// <summary>
+    /// Utilities for Control.
+    /// </summary>
+    public class ControlUtils
+    {
+        /// <summary>
+        /// Use ControlPaint.LightLight to lighten the foreground of the control.
+        /// </summary>
+        /// <param name="ctrl">Control object.</param>
+        public static void ControlForeColorLight(Control ctrl) {
+            ctrl.ForeColor = ControlPaint.LightLight(ctrl.ForeColor);
+        }
+        
+        /// <summary>
+        /// Use ControlPaint.DarkDark to darken the foreground of the control.
+        /// </summary>
+        /// <param name="ctrl">Control object.</param>
+        public static void ControlForeColorDark(Control ctrl) {
+            ctrl.ForeColor = ControlPaint.DarkDark(ctrl.ForeColor);
+        }
+        
+        /// <summary>
+        /// Use ControlPaint.LightLight to lighten the foreground of the control.
+        /// </summary>
+        public static void ControlForeColorLightHandle(object sender, EventArgs e) {
+            ControlForeColorLight((Control) sender);
+        }
+        
+        /// <summary>
+        /// Use ControlPaint.DarkDark to darken the foreground of the control.
+        /// </summary>
+        public static void ControlForeColorDarkHandle(object sender, EventArgs e) {
+            ControlForeColorDark((Control) sender);
+        }
+        
+        /// <summary>
+        /// Make the foreground color of the control change automatically, like a button.
+        /// </summary>
+        /// <param name="ctrl">Control object.</param>
+        public static void ControlForceColorAutoChange(Control ctrl) {
+            ctrl.MouseEnter += new EventHandler(ControlForeColorDarkHandle);
+            ctrl.MouseLeave += new EventHandler(ControlForeColorLightHandle);
+        }
+        
+        /// <summary>
+        /// Use ControlPaint.LightLight to lighten the background of the control.
+        /// </summary>
+        /// <param name="ctrl">Control object.</param>
+        public static void ControlBackColorLight(Control ctrl) {
+            ctrl.BackColor = ControlPaint.LightLight(ctrl.BackColor);
+        }
+        
+        /// <summary>
+        /// Use ControlPaint.DarkDark to darken the background of the control.
+        /// </summary>
+        /// <param name="ctrl">Control object.</param>
+        public static void ControlBackColorDark(Control ctrl) {
+            ctrl.BackColor = ControlPaint.DarkDark(ctrl.BackColor);
+        }
+        
+        /// <summary>
+        /// Use ControlPaint.LightLight to lighten the background of the control.
+        /// </summary>
+        public static void ControlBackColorLightHandle(object sender, EventArgs e) {
+            ControlBackColorLight((Control) sender);
+        }
+        
+        /// <summary>
+        /// Use ControlPaint.DarkDark to darken the background of the control.
+        /// </summary>
+        public static void ControlBackColorDarkHandle(object sender, EventArgs e) {
+            ControlBackColorDark((Control) sender);
+        }
+        
+        /// <summary>
+        /// Make the background color of the control change automatically, like a button.
+        /// </summary>
+        /// <param name="ctrl">Control object.</param>
+        public static void ControlBackColorAutoChange(Control ctrl) {
+            ctrl.MouseEnter += new EventHandler(ControlBackColorDarkHandle);
+            ctrl.MouseLeave += new EventHandler(ControlBackColorLightHandle);
+        }
+    }
+}
---
+++ Rgbit/DotNet/WinFormsUtils/FormUtils.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * Copyright (c) 2018 by rgb-24bit.
+ * License MIT, see LICENSE for more details.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Rgbit.DotNet.WinFormsUtils
+{
+    /// <summary>
+    /// Utilities for Form.
+    /// </summary>
+    public class FormUtils
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        
+        /// <summary>
+        /// Practical mouse moves Form without borders.
+        /// </summary>
+        /// <param name="form">The form to be moved.</param>
+        public static void FormMourseMove(Form form) {
+            ReleaseCapture();
+            SendMessage(form.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+        
+        /// <summary>
+        /// Practical mouse moves Form without borders.
+        /// </summary>
+        public static void FormMouseMoveHandle(object sender, EventArgs e) {
+            FormMourseMove((Form) sender);
+        }
+    }
+}
