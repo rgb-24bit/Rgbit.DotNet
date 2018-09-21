0d04445a796abb9368ea9178c4ebed2bfe866c7d
rgb-24bit <rgb-24bit@foxmail.com>
2018-09-21 14:21:06 +0800

    feat: the basic functions of MCIPlayer are completed.

--- Rgbit/DotNet/MediaUtils/MCIPlayer.cs
+++ Rgbit/DotNet/MediaUtils/MCIPlayer.cs
@@ -12,6 +12,10 @@ namespace Rgbit.DotNet.MediaUtils
     /// <summary>
     /// Use MCI to playing multimedia devices and recording multimedia resource files.
     /// 
+    /// Method related to playback behavior: Play, Stop, Pause, Resume.
+    /// Ways to get song information: IsPlaying, GetCurentMilisecond, GetTotalLength.
+    /// More methods: Execute, GetErrorMessage, SetPosition, SwicthTarget.
+    /// 
     /// Reference https://docs.microsoft.com/en-us/windows/desktop/multimedia/mci
     /// Reference https://www.codeproject.com/Articles/63094/Simple-MCI-Player
     /// </summary>
@@ -26,8 +30,7 @@ namespace Rgbit.DotNet.MediaUtils
         [DllImport("winmm.dll")]
         public static extern int mciGetErrorString(int errCode, StringBuilder errMsg, int buflen);
         
-        public MCIPlayer(string target)
-        {
+        public MCIPlayer(string target) {
             this.target = target;
         }
         
@@ -73,9 +76,9 @@ namespace Rgbit.DotNet.MediaUtils
         /// Open the target file and automatically call it when playing.
         /// </summary>
         /// <returns>Return true for success, false for failure.</returns>
-        public bool Open() {
+        private bool Open() {
             this.Close();  // Look a little longer
- 
+            
             string command = string.Format("open {0} type mpegvideo", target);
             errorCode = mciSendString(command, null, 0, IntPtr.Zero);
             
@@ -91,14 +94,14 @@ namespace Rgbit.DotNet.MediaUtils
         /// Turn off playback,
         /// </summary>
         /// <returns>Return true for success, false for failure.</returns>
-        public bool Close() {
+        private bool Close() {
             string command = "play " + target;
             errorCode = mciSendString(command, null, 0, IntPtr.Zero);
             return errorCode == 0;
         }
         
         /// <summary>
-        /// Play target file.
+        /// The play command starts playing a device.
         /// </summary>
         /// <param name="options">
         /// Playback options, use like play("repeat", "fast").
@@ -123,6 +126,90 @@ namespace Rgbit.DotNet.MediaUtils
             }
             
             return errorCode == 0;
+        }
+        
+        /// <summary>
+        /// The stop command stops playback. 
+        /// 
+        /// Turn off music, please use this method instead of Close.
+        /// </summary>
+        /// <returns>Return true for success, false for failure.</returns>
+        public bool Stop() {
+            String command = string.Format("stop {0}", target);
+            errorCode = mciSendString(command, null, 0, IntPtr.Zero);
+            return errorCode == 0;
+        }
+        
+        /// <summary>
+        /// The pause command pauses playing.
+        /// </summary>
+        /// <returns>Return true for success, false for failure.</returns>
+        public bool Pause() {
+            String command = string.Format("pause {0}", target);
+            errorCode = mciSendString(command, null, 0, IntPtr.Zero);
+            return errorCode == 0;
+        }
+        
+        /// <summary>
+        /// The resume command continues playing on a device
+        /// that has been paused using the pause command.
+        /// </summary>
+        /// <returns>Return true for success, false for failure.</returns>
+        public bool Resume() {
+            String command = string.Format("resume {0}", target);
+            errorCode = mciSendString(command, null, 0, IntPtr.Zero);
+            return errorCode == 0;
+        }
+        
+        /// <summary>
+        /// Determine if the current media is playing.
+        /// </summary>
+        public bool IsPlaying() {
+            string command = string.Format("status {0} mode", target);
+            StringBuilder returnData = new StringBuilder(128);
+            errorCode = mciSendString(command, returnData, 128, IntPtr.Zero);
+            
+            if(returnData.ToString().Contains("playing")) {
+                return true;
+            }
+            
+            return false;
+        }
+        
+        /// <summary>
+        /// Get the position of the song in milliseconds.
+        /// </summary>
+        public int GetCurentMilisecond() {
+            string command = string.Format("status {0} position", target);
+            StringBuilder returnData = new StringBuilder(128);
+            errorCode = mciSendString(command, returnData, 128, IntPtr.Zero);
+            return int.Parse(returnData.ToString());
+        }
+        
+        /// <summary>
+        /// Set the position of the song in milliseconds.
+        /// </summary>
+        /// <param name="miliseconds">The position of the song in milliseconds.</param>
+        /// <returns>Return true for success, false for failure.</returns>
+        public bool SetPosition(int miliseconds) {
+            if (IsPlaying()) {
+                string command = string.Format("play {0} from {1}", target, miliseconds);
+                errorCode = mciSendString(command, null, 0, IntPtr.Zero);
+            } else {
+                string command = string.Format("seek {0} to {1}", target, miliseconds);
+                errorCode = mciSendString(command, null, 0, IntPtr.Zero);
+            }
+            return errorCode == 0;
+        }
+        
+        /// <summary>
+        /// Get the length of the song.
+        /// </summary>
+        public int GetTotalLength() {
+            string command = string.Format("status {0} length", target);
+            StringBuilder returnData = new StringBuilder(128);
+            errorCode = mciSendString(command, returnData, 128, IntPtr.Zero);
+            return int.Parse(returnData.ToString());
         }
     }
 }
