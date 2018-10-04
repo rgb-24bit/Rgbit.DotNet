2f24533d13fc11f6b853b113407017255c234d60
rgb-24bit <rgb-24bit@foxmail.com>
2018-10-04 15:08:42 +0800

    feat: make SwapRgb and SwapArgb Memory processing

--- Rgbit/DotNet/DrawUtils/ImageProcess.cs
+++ Rgbit/DotNet/DrawUtils/ImageProcess.cs
@@ -7,40 +7,72 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace Rgbit.DotNet.DrawUtils
 {
     /// <summary>
     /// Image Process utils.
-    /// 
-    /// TODO Make SwapRgb and SwapArgb Memory processing
     /// </summary>
     public class ImageProcess
     {
         /// <summary>
+        /// Determine whether the image format is one of the limited formats.
+        /// </summary>
+        /// <param name="bitmap">Picture object.</param>
+        /// <param name="formats">Image Format.</param>
+        /// <returns>Return true in qualified format, false otherwise.</returns>
+        private static bool CheckImageFormat(Bitmap bitmap, params ImageFormat[] formats) {
+            foreach (ImageFormat format in formats) {
+                if (bitmap.RawFormat.Equals(format)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        /// <summary>
         /// Swap image rgb color channel.
         /// </summary>
         /// <param name="image">Image object.</param>
-        /// <param name="rgb">The order of the new color channels, such as "grb".</param>
+        /// <param name="rgbOrder">The order of the new color channels, such as "grb".</param>
         /// <returns>Swaped image object</returns>
-        public static Image SwapRgb(Image image, String rgb) {
+        public static Image SwapRgb(Image image, String rgbOrder) {
             Bitmap bitmap = image.Clone() as Bitmap;
             
-            Dictionary<char, Func<Color, byte>> map = new Dictionary<char, Func<Color, byte>>();
-            
-            map.Add('r', (p) => p.R);
-            map.Add('g', (p) => p.G);
-            map.Add('b', (p) => p.B);
-            
-            for (int x = 0; x < bitmap.Width; x++) {
-                for (int y = 0; y < bitmap.Height; y++) {
-                    Color pixel = bitmap.GetPixel(x, y);
-                    bitmap.SetPixel(x, y, Color.FromArgb(
-                        map[rgb[0]](pixel),
-                        map[rgb[1]](pixel),
-                        map[rgb[2]](pixel)));
-                }
+            if (!CheckImageFormat(bitmap, ImageFormat.Bmp, ImageFormat.Jpeg, ImageFormat.Png)) {
+                throw new ArgumentException("Unsuported format, only support for bmp, jpg or png");
             }
+            
+            // Locks the bitmap into system memory.
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            BitmapData bmpdata = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            IntPtr ptr = bmpdata.Scan0;
+            
+            // Declare an array to hold the bytes of the bitmap.
+            int totalPixels = Math.Abs(bmpdata.Stride) * bitmap.Height;
+            byte[] rgbValues = new byte[totalPixels];
+            
+            // Copy the RGB values into the array.
+            Marshal.Copy(ptr, rgbValues, 0, totalPixels);
+            
+            int b = 0, g = 1, r = 2;  // BGR
+            int increase = CheckImageFormat(bitmap, ImageFormat.Png) ? 4 : 3;  // PNG is BGRA
+
+            for (int i = 0; i < totalPixels; i += increase) {
+                Dictionary<char, byte> map = new Dictionary<char, byte>() {
+                    {'r', rgbValues[r + i]},
+                    {'g', rgbValues[g + i]},
+                    {'b', rgbValues[b + i]}
+                };
+                
+                rgbValues[r + i] = map[rgbOrder[0]];
+                rgbValues[g + i] = map[rgbOrder[1]];
+                rgbValues[b + i] = map[rgbOrder[2]];
+            }
+            
+            Marshal.Copy(rgbValues, 0, ptr, totalPixels);
+            bitmap.UnlockBits(bmpdata);
             
             return bitmap.Clone() as Image;
         }
@@ -49,28 +81,45 @@ namespace Rgbit.DotNet.DrawUtils
         /// Swap image argb color channel.
         /// </summary>
         /// <param name="image">Image object.</param>
-        /// <param name="argb">The order of the new color channels, such as "agrb".</param>
+        /// <param name="argbOrder">The order of the new color channels, such as "agrb".</param>
         /// <returns>Swaped image object</returns>
-        public static Image SwapArgb(Image image, string argb) {
+        public static Image SwapArgb(Image image, string argbOrder) {
             Bitmap bitmap = image.Clone() as Bitmap;
             
-            Dictionary<char, Func<Color, byte>> map = new Dictionary<char, Func<Color, byte>>();
-            
-            map.Add('a', (p) => p.A);
-            map.Add('r', (p) => p.R);
-            map.Add('g', (p) => p.G);
-            map.Add('b', (p) => p.B);
-            
-            for (int x = 0; x < bitmap.Width; x++) {
-                for (int y = 0; y < bitmap.Height; y++) {
-                    Color pixel = bitmap.GetPixel(x, y);
-                    bitmap.SetPixel(x, y, Color.FromArgb(
-                        map[argb[0]](pixel),
-                        map[argb[1]](pixel),
-                        map[argb[2]](pixel),
-                        map[argb[3]](pixel)));
-                }
+            if (!CheckImageFormat(bitmap, ImageFormat.Png)) {
+                throw new ArgumentException("Unsuported format, only support png");
             }
+            
+            // Locks the bitmap into system memory.
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            BitmapData bmpdata = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            IntPtr ptr = bmpdata.Scan0;
+            
+            // Declare an array to hold the bytes of the bitmap.
+            int totalPixels = Math.Abs(bmpdata.Stride) * bitmap.Height;
+            byte[] rgbValues = new byte[totalPixels];
+            
+            // Copy the ARGB values into the array.
+            Marshal.Copy(ptr, rgbValues, 0, totalPixels);
+            
+            int b = 0, g = 1, r = 2, a = 3;  // BGRA
+
+            for (int i = 0; i < totalPixels; i += 4) {
+                Dictionary<char, byte> map = new Dictionary<char, byte>() {
+                    {'r', rgbValues[r + i]},
+                    {'g', rgbValues[g + i]},
+                    {'b', rgbValues[b + i]},
+                    {'a', rgbValues[a + i]}
+                };
+                
+                rgbValues[r + i] = map[argbOrder[0]];
+                rgbValues[g + i] = map[argbOrder[1]];
+                rgbValues[b + i] = map[argbOrder[2]];
+                rgbValues[a + i] = map[argbOrder[3]];
+            }
+            
+            Marshal.Copy(rgbValues, 0, ptr, totalPixels);
+            bitmap.UnlockBits(bmpdata);
             
             return bitmap.Clone() as Image;
         }
@@ -83,24 +132,34 @@ namespace Rgbit.DotNet.DrawUtils
         public static Image Gray(Image image) {
             Bitmap bitmap = image.Clone() as Bitmap;
             
+            if (!CheckImageFormat(bitmap, ImageFormat.Bmp, ImageFormat.Jpeg, ImageFormat.Png)) {
+                throw new ArgumentException("Unsuported format, only support for bmp, jpg or png");
+            }
+            
+            // Locks the bitmap into system memory.
             Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
             BitmapData bmpdata = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
             IntPtr ptr = bmpdata.Scan0;
             
-            int bytes = bitmap.Width * bitmap.Height * 3;
-            byte[] rgbValues = new byte[bytes];
+            // Declare an array to hold the bytes of the bitmap.
+            int totalPixels = Math.Abs(bmpdata.Stride) * bitmap.Height;
+            byte[] rgbValues = new byte[totalPixels];
             
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            // Copy the RGB values into the array.
+            Marshal.Copy(ptr, rgbValues, 0, totalPixels);
             
-            for (int i = 0; i < rgbValues.Length; i += 3) {
-                int r = rgbValues[i], g = rgbValues[i + 1], b = rgbValues[i + 2];
-                byte gray = (byte) ((r * 299 + g * 587 + b * 114 + 500) / 1000);
-                rgbValues[i] = rgbValues[i + 1] = rgbValues[i + 2] = gray;
+            int b = 0, g = 1, r = 2;  // BGR
+            int increase = CheckImageFormat(bitmap, ImageFormat.Png) ? 4 : 3;  // PNG is BGRA
+            
+            for (int i = 0; i < totalPixels; i += increase) {
+                int rv = rgbValues[r + i], gv = rgbValues[g + i], bv = rgbValues[b + i];
+                byte gray = (byte) ((rv * 299 + gv * 587 + bv * 114 + 500) / 1000);
+                rgbValues[r + i] = rgbValues[g + i] = rgbValues[b + i] = gray;
             }
             
-            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-            
+            Marshal.Copy(rgbValues, 0, ptr, totalPixels);
             bitmap.UnlockBits(bmpdata);
+            
             return bitmap.Clone() as Image;
         }
         
@@ -110,22 +169,34 @@ namespace Rgbit.DotNet.DrawUtils
         public static Image Invert(Image image) {
             Bitmap bitmap = image.Clone() as Bitmap;
             
+            if (!CheckImageFormat(bitmap, ImageFormat.Bmp, ImageFormat.Jpeg, ImageFormat.Png)) {
+                throw new ArgumentException("Unsuported format, only support for bmp, jpg or png");
+            }
+            
+            // Locks the bitmap into system memory.
             Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
             BitmapData bmpdata = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
             IntPtr ptr = bmpdata.Scan0;
             
-            int bytes = bitmap.Width * bitmap.Height * 3;
-            byte[] rgbValues = new byte[bytes];
+            // Declare an array to hold the bytes of the bitmap.
+            int totalPixels = Math.Abs(bmpdata.Stride) * bitmap.Height;
+            byte[] rgbValues = new byte[totalPixels];
             
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            // Copy the RGB values into the array.
+            Marshal.Copy(ptr, rgbValues, 0, totalPixels);
             
-            for (int i = 0; i < rgbValues.Length; i++) {
-                rgbValues[i] = (byte) (rgbValues[i] ^ 0xFF);
+            int b = 0, g = 1, r = 2;  // BGR
+            int increase = CheckImageFormat(bitmap, ImageFormat.Png) ? 4 : 3;  // PNG is BGRA
+            
+            for (int i = 0; i < totalPixels; i += increase) {
+                rgbValues[r + i] ^= 0xFF;  // x + (x ^ 0xFF) = 0xFF
+                rgbValues[g + i] ^= 0xFF;
+                rgbValues[b + i] ^= 0xFF;
             }
             
-            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-            
+            Marshal.Copy(rgbValues, 0, ptr, totalPixels);
             bitmap.UnlockBits(bmpdata);
+            
             return bitmap.Clone() as Image;
         }
     }
